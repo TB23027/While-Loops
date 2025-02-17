cb0df2274b58e93fa09e04c0f60d0744473308af
Torr Beaman <tb23027@my.westlake.school.nz>
2025-02-18 12:36:19 +1300

    Done task 3

--- WhileLoop.py
+++ WhileLoop.py
@@ -9,3 +9,18 @@ while question_1 > 0:
     question_1 -=1
 print("BLAST OFF!")
 
+
+total_sum = 0
+question_2 = int(input("Enter a number as many times as you would like until you want to stop, enter 0: "))
+while True:
+    try:
+        number = int(input("Enter a number as many times as you would like until you want to stop, enter 0: "))
+        if number == 0:
+            break
+        total_sum += number
+    except ValueError:
+        print("Please enter a valid integer.")
+
+print(f"Total sum: {total_sum}")
+
+    
