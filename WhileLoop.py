c76e5c532c62f56398ede8e44ac9b54d6a277ee7
Torr Beaman <tb23027@my.westlake.school.nz>
2025-02-18 12:25:01 +1300

    Finished Task 2

--- WhileLoop.py
+++ WhileLoop.py
@@ -1,3 +1,11 @@
-for i in range(1,11):
-    print(i)
+number = 1
+while number < 11:
+    print(number)
+    number+=1
+
+question_1 = int(input("Enter a number: "))
+while question_1 > 0:
+    print(question_1)
+    question_1 -=1
+print("BLAST OFF!")
 
